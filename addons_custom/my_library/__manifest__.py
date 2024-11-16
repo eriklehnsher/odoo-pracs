a28d0af55cf6363f20693577dfa527a5f4a7c537
Xuan Hoa <75961133+eriklehnsher@users.noreply.github.com>
2024-11-16 16:35:41 +0700

    adding: my_library module

---
+++ addons_custom/my_library/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name':'My Library',
+    'summary':'Manage books easily',
+    'description': """
+    Manage Library
+    ==============
+    Description related to library
+    """,
+    'author':'Erik.',
+    'version':'1.0.0',
+    'depend':['base'],
+    'data':[
+        "views/my_library_views.xml",
+        
+        ],
+
+}
