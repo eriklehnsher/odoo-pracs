af0b50c164ce60fe0b48770f8156dc44ba199aa2
Xuan Hoa <75961133+eriklehnsher@users.noreply.github.com>
2024-11-17 20:38:41 +0700

    adding:library moulde

--- addons_custom/my_library/__manifest__.py
+++ addons_custom/my_library/__manifest__.py
@@ -10,8 +10,9 @@
     'version':'1.0.0',
     'depend':['base'],
     'data':[
+        "security/groups.xml",
+        "security/ir.model.access.csv",
         "views/my_library_views.xml",
-        
         ],
 
 }
--- addons_custom/my_library/models/library_book.py
+++ addons_custom/my_library/models/library_book.py
@@ -3,7 +3,14 @@ from odoo import models, fields, api
 
 class LibraryBook(models.Model):
     _name = 'library.book'
-
+    _description = 'Library Book'
+    #to sort the records first-from the newest to the oldest + then by title
+    _order = 'date_release desc, name'
+    #to use the short_name field as the record representation - ở đây là để hiển thị tên ngắn của sách
+    _rec_name = 'short_name'
+    
+    short_name = fields.Char('Short Title', required=True)
     name = fields.Char('Title', required=True)
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many('res.partner', string='Author')
+
