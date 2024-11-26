33b0510f3fe4da7d876795fdfea907bc7d4a4524
Lưu Xuân Hòa (HN-BCD-Developer) <hoalx@runsystem.local>
2024-11-26 12:35:59 +0700

    create inventory_pracs module

---
+++ addons_custom/inventory_pracs/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Inventory Practices",
+    "summary": "Inventory Practices",
+    "version": "1.0",
+    "category": "Inventory",
+    "author": "Erik.",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        # "views/stock_picking_inherit_view.xml",
+        # "views/stock_move_inherit_view.xml",
+    ],
+}
--- addons_custom/plan_sales/models/plan_sale_order.py
+++ addons_custom/plan_sales/models/plan_sale_order.py
@@ -1,7 +1,7 @@
 from email.policy import default
 
 from odoo import models, fields, api
-
+from odoo.exceptions import UserError
 
 class PlanSaleOrder(models.Model):
     _name = 'plan.sale.order'
@@ -32,8 +32,76 @@ class PlanSaleOrder(models.Model):
     # all defined methods here
 
     def action_approve_plan_sale_order(self):
-       pass
+        current_user = self.env.user.partner_id
+        if current_user in self.approver_ids and current_user not in self.approved_by_ids:
+            self.approved_by_ids = [(4, current_user.id)]
+            remaining_approvers = self.approver_ids - self.approved_by_ids
+            self.message_post(
+                body=f" Người Phê Duyệt {current_user.name} đã phê duyệt phương án kinh doanh",
+                message_type='notification',
+                partner_ids = [current_user.id]
+            )
+            if len(self.approved_by_ids) == len(self.approver_ids):
+                self.state = 'approved'
+                self.message_post(
+                    body=f"Phương Án Kinh Doanh đã được tất cả phê duyệt",
+                    message_type='notification',
+                    partner_ids = [self.creator_id.partner_id.id]
+                )
+            else:
+                self.state = 'pending'
+                remaining_name = ', '.join([approver.name for approver in remaining_approvers])
+                self.message_post(
+                    body=f"Chờ phê duyệt từ {remaining_name}",
+                    message_type='notification',
+                    partner_ids = [remaining_approvers.ids]
+                )
+        else:
+            raise UserError('Bạn không có quyền phê duyệt phương án này')
+
+
+
+
 
     def action_reject_plan_sale_order(self):
-        pass
+        current_user = self.env.user.partner_id
+        if current_user in self.approver_ids and current_user not in self.rejected_by_ids:
+            self.rejected_by_ids = [(4, current_user.id)]
+            remaining_rejectors = self.approver_ids - self.rejected_by_ids
+            self.state = "rejected"
+            self.message_post(
+                body=f"Người phê duyệt {current_user.name} đã từ chối kế hoạch.",
+                message_type="notification",
+                partner_ids=[current_user.id]
+            )
+            if len(self.rejected_by_ids) == len(self.approver_ids):
+                self.state = "rejected"
+                self.message_post(
+                    body="Kế hoạch đã được tất cả người phê duyệt từ chối.",
+                    message_type="notification",
+                    partner_ids=[self.creater_id.partner_id.id]
+                )
+            else:
+                self.state = "pending"
+                remaining_names = ", ".join(
+                    [rejector.name for rejector in remaining_rejectors])
+                self.message_post(
+                    body=f"Kế hoạch đã bị từ chối bởi {current_user.name}. Đang chờ {remaining_names} .",
+                    message_type="notification",
+                    partner_ids=[self.creater_id.partner_id.id]
+                )
+        else:
+            raise UserError("Người phê duyệt không hợp lệ hoặc đã từ chối.")
 
+    def action_sent_for_approval(self):
+        if not self.approver_ids:
+            raise UserError("Vui lòng chọn người phê duyệt")
+        self.state = "pending"
+
+        for approver in self.approver_ids:
+            self.message_post(
+                body="Kế hoạch đã được gửi phê duyệt.",
+                message_type="notification",
+                partner_ids=[approver.id],
+            )
+        return True
